05226a054067a57e6aad80f8e42789a2edf074ab
Alberto Zuin <alberto@azns.it>
2014-07-29 10:42:13 +0100

    Temporary disabled blog

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -4,19 +4,19 @@ class StaticsController < ApplicationController
   end
 
   def create
-    if params[:opt_in]
+    if params[:user][:opt_in] == '1'
       @user = User.new(user_attributes)
       if @user.save
         SubscribeMailer.new_subscription(@user)
         flash[:notice] = @user.first_name.to_s + ', thank you for subscribe'
         redirect_to root_path
       else
-        flash[:error] = 'Sorry, something went wrong: ' + subscriber.errors.full_messages.to_sentence
+        flash[:error] = 'Sorry, something went wrong: ' + @user.errors.full_messages.to_sentence
         redirect_to root_path
       end
     else
       @user=params[:user]
-      SubscribeMailer.new_message(@user[first_name],@user[:last_name],@user[:email],@user[:message])
+      SubscribeMailer.new_message(@user[:first_name],@user[:last_name],@user[:email],@user[:message])
       flash[:notice] = 'Thank you for your message'
       redirect_to root_path
     end
