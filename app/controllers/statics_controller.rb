2ab598d04f494288f984966fd1ac02730599055b
Alberto Zuin <alberto@azns.it>
2014-07-29 11:14:41 +0100

    Temporary disabled blog

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -9,16 +9,16 @@ class StaticsController < ApplicationController
       if @user.save
         SubscribeMailer.new_subscription(@user,params[:user][:message])
         flash[:notice] = @user.first_name.to_s + ', thank you for subscribe'
-        redirect_to root_path(anchor: 'contact')
+        redirect_to root_path(anchor: 'contact-top')
       else
         flash[:error] = 'Sorry, something went wrong: ' + @user.errors.full_messages.to_sentence
-        redirect_to root_path(anchor: 'contact')
+        redirect_to root_path(anchor: 'contact-top')
       end
     else
       @user=params[:user]
       SubscribeMailer.new_message(@user[:first_name],@user[:last_name],@user[:email],@user[:message])
       flash[:notice] = 'Thank you for your message'
-      redirect_to root_path(anchor: 'contact')
+      redirect_to root_path(anchor: 'contact-top')
     end
   end
 
