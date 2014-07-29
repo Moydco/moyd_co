63f2540ac74e0a977350e4c420569ded49266a0a
Alberto Zuin <alberto@azns.it>
2014-07-29 10:44:46 +0100

    Temporary disabled blog

--- app/mailers/subscribe_mailer.rb
+++ app/mailers/subscribe_mailer.rb
@@ -3,15 +3,14 @@ class SubscribeMailer < ActionMailer::Base
 
   def new_subscription(user)
     @user = user
-    mail(to: 'hello@handmade.io', subject: 'New user').deliver
+    mail(to: 'hello@moyd.co', subject: 'New user').deliver
   end
 
-  def new_message(first_name, last_name, email, to ,message)
+  def new_message(first_name, last_name, email ,message)
     @first_name = first_name
     @last_name  = last_name
     @email      = email
-    @to         = to
     @message    = message
-    mail(to: 'hello@handmade.io', subject: 'New message').deliver
+    mail(to: 'hello@moyd.co', subject: 'New message').deliver
   end
 end
