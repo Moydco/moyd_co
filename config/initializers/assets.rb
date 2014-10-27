ecbeccbb938001a86fbf3aed34ecfcbf4950025b
Alberto Zuin <alberto@azns.it>
2014-10-27 10:52:39 +0000

    New website with login and online payment

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css.sass, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( statics.css statics.js zoomico.png blog.css swipebox.css blue.css animate.min.css )
+Rails.application.config.assets.precompile += %w( statics.css statics.js zoomico.png blog.css swipebox.css blue.css.scss animate.min.css )
