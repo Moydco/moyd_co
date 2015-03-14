5d4b3c7e00e99f4db7253aa425de7c04bba9c652
Administrator <alberto@azns.it>
2015-03-14 18:28:27 +0000

    Deleted keys

--- config/initializers/gibbon.rb
+++
@@ -1,3 +0,0 @@
-Gibbon::API.api_key = '539b5c32c6b49c3c5e2d326140b2d940-us5'
-Gibbon::API.timeout = 15
-Gibbon::API.throws_exceptions = false
--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.stripe = {
-    :publishable_key => Settings.stripe_public_key,
-    :secret_key      => Settings.stripe_api_key
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
