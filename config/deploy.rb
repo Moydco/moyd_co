34aff88a7dd76b43e95ce322ce0125e3aebbf056
Administrator <alberto@azns.it>
2015-03-16 11:00:55 +0000

    hosting and reseller

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, 'moyd.co'
-set :repo_url, 'git@git.azcloud.it:alberto/moyd_co.git'
+set :repo_url, 'https://github.com/Moydco/moyd_co.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 # set :scm, :git
@@ -13,7 +13,7 @@ set :repo_url, 'git@git.azcloud.it:alberto/moyd_co.git'
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
-set :linked_files, %w{config/settings.local.yml config/mongoid.yml}
+set :linked_files, %w{config/settings.local.yml config/mongoid.yml config/newrelic.yml config/unicorn.rb config/initializers/devise.rb config/initializers/gibbon.rb config/initializers/quickbooks.rb config/initializers/stripe.rb}
 
 namespace :deploy do
 
